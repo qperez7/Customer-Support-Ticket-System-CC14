6c55177b05ca92ca05cd27abcd38d563c15f10d2
Quirianyailet Perez <qperez1@usf.edu>
2024-11-01 17:10:48 -0400

    Implement async/await to fetch ticket data and handle errors

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
 // Task 1: Setup HTML Structure for the Ticket System (Refer to html file)
 
+// Task 2: Fetch Tickets Using Async/Await and Handle Errors
+async function checkInventory(productId) {
+    try {
+        const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${productId}`);
+        
+        if (!response.ok) {
+            throw new Error('Tickets not found');
+        }
+
+        const product = await response.json();
+        return product;
+    } catch (error) {
+        console.error('Error:', error.message);
+    }
+}
