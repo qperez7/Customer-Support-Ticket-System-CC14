f5a514f8086f428ea339412e6ce0f0a6cb427005
Quirianyailet Perez <qperez1@usf.edu>
2024-11-02 12:09:36 -0400

    Use finally to clean up after the fetch.

--- main.js
+++ main.js
@@ -6,32 +6,31 @@ const ticketList = document.getElementById('displayingTickets');
 // Async function to fetch Unresolved Tickets
 async function displayUnresolvedTickets(id) {
     try {
-        const ticketResponse = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+        const ticketResponse = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`); 
         if (!ticketResponse.ok) {
             throw new Error('Failed to fetch ticket data');
         }
         const ticketId = await ticketResponse.json();
-        console.log('Ticket ID:', ticketId);
-
-        const customerResponse = await fetch(`https://jsonplaceholder.typicode.com/posts/${userId}`);
-        if (!customerResponse.ok) {
-            throw new Error('Failed to fetch customers');
-        }
-        const userId = await ordersResponse.json();
-        console.log('Customer Name:', userId);
+        console.log('Ticket ID:', ticketId); // Only ticket by id are displayed in console for filtering
+        
         // Task 3: Display Tickets Dynamically on the Page
-        ticketId.forEach(ticket => {
-            const listItem = document.createElement('li');
-            listItem.textContent = `Ticket Id: ${ticket.id}, Info: ${ticket.userId} ${ticket.title} ${ticket.body}`;
-            ticketList.appendChild(listItem);
-        });
+         const apiUrl = 'https://jsonplaceholder.typicode.com/posts';
+         
+         fetch(apiUrl)
+         .then(response => response.json())
+         .then(tickets => {
+             tickets.forEach(ticket => {
+             const listItem = document.createElement('li');
+             listItem.textContent = `Ticket Id: ${ticket.id}, Customer Name: ${ticket.userId}, Issue: ${ticket.title}, Details: ${ticket.body}`;
+             ticketList.appendChild(listItem); // All tickets are displayed on the page
+         });
+     })
     } catch (error) {
-        console.error('Error:', error.message);
+        console.error('Error:', error.message); // Catches any errors 
     }
     // Task 4: Use finally to Ensure Cleanup
     finally {
-    console.log('Closing Unresolved Tickets');
-}}
-
-
-displayUnresolvedTickets(1)
+        console.log('Closing Unresolved Tickets'); // Displays message: 'Closing Unresolved Tickets'
+}
+    }
+    displayUnresolvedTickets(1); // This console logs ticket 1 (Works with Task 2)
